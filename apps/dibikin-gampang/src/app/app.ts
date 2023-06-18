bb956c5612755ee297d2c715308d2fae249842e2
callmehandsomeboy <adhamkukuh@gmail.com>
2023-06-18 17:06:00 +0700

    setup launcher bot and add telelgraf package

---
+++ apps/dibikin-gampang/src/app/app.ts
@@ -0,0 +1,10 @@
+import { Telegraf } from 'telegraf';
+
+export async function App(
+  bot: Telegraf,
+  sheetId: string,
+  clientEmail: string,
+  privateKey: string
+) {
+  return bot;
+}
--- apps/dibikin-gampang/src/main.ts
+++ apps/dibikin-gampang/src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import { Telegraf } from 'telegraf';
+import { environment } from './environments/environment';
+import { App } from './app/app';
+(async () => {
+  console.log('Start bot ...');
+  const bot = new Telegraf(environment.token);
+  const start = await App(
+    bot,
+    environment.sheet_id,
+    environment.client_email,
+    environment.private_key
+  );
+  start.launch();
+})();
